9ffcfeb429a5467668c87d2626939ddb35271713
nathanxcode <codexnathan7@gmail.com>
2023-10-12 23:40:41 -0300

    Implementing add, remove and toString methods to RingBuffer

--- ringbuffer/src/main/java/com/ringbuffer/Main.java
+++ ringbuffer/src/main/java/com/ringbuffer/Main.java
@@ -2,6 +2,21 @@ package com.ringbuffer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        RingBuffer<String> myRingBuffer = new RingBuffer<>();
+        myRingBuffer.add("D");
+        myRingBuffer.add("L");
+        myRingBuffer.add("R");
+        myRingBuffer.add("O");
+        myRingBuffer.add("W");
+        System.out.println(myRingBuffer.size());
+        System.out.println(myRingBuffer.isEmpty());
+        System.out.println(myRingBuffer);
+        myRingBuffer.remove(0);
+        myRingBuffer.remove(1);
+        myRingBuffer.remove(2);
+        System.out.println(myRingBuffer.isEmpty());
+        System.out.println(myRingBuffer);
+
     }
 }
--- ringbuffer/src/main/java/com/ringbuffer/RingBuffer.java
+++ ringbuffer/src/main/java/com/ringbuffer/RingBuffer.java
@@ -7,9 +7,48 @@ public class RingBuffer<T> {
     int listSize;
 
     public RingBuffer() {
-
+        this.head = null;
+        this.tail = null;
         this.listSize = 0;
 
+    }
+
+    public void add(T content) {
+        Node<T> newNode = new Node<>(content);
+
+        if (this.isEmpty()) {
+            this.head = newNode;
+            this.tail = head;
+            this.head.setNextNode(tail);
+        } else {
+            newNode.setNextNode(this.tail);
+            this.head.setNextNode(newNode);
+            this.tail = newNode;
+        }
+        this.listSize++;
+
+    }
+
+    public void remove(int index) {
+
+        if (index >= size())
+            throw new IndexOutOfBoundsException("Index is bigger than list size.");
+
+        Node<T> auxNode = tail;
+        if (index == 0) {
+            this.tail = tail.getNextNode();
+            this.head.setNextNode(this.tail);
+
+        } else if (index == 1) {
+
+            this.tail.setNextNode(this.tail.getNextNode());
+        } else {
+            for (int i = 0; i < index - 1; i++) {
+                auxNode = auxNode.getNextNode();
+            }
+            auxNode.setNextNode(auxNode.getNextNode().getNextNode());
+        }
+        this.listSize--;
     }
 
     public T get(int index) {
@@ -40,6 +79,23 @@ public class RingBuffer<T> {
 
     public int size() {
         return this.listSize;
+    }
+
+    public String toString() {
+        String str = "";
+
+        Node<T> auxNode = this.tail;
+        for (int i = 0; i < size(); i++) {
+
+            str += "Node[content{ " + auxNode.getContent() + " }] -> ";
+
+            auxNode = auxNode.getNextNode();
+
+        }
+
+        str += this.size() != 0 ? "(back to start)" : "[]";
+
+        return str;
     }
 
 }
